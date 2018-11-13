5afbda02f29fa974019652ebfef41b5922b5e319
Mike Szczys <szczys@hotmail.com>
2018-11-12 23:19:20 -0600

    Added output messages

--- findAndMove.py
+++ findAndMove.py
@@ -14,10 +14,15 @@ def main():
     epFilename = findEpisodeData("Late Night with Seth Meyers","Claire Foy Lucas Hedges boygenius Franklin Vanderbilt")
     if epFilename != "0":
         if os.path.isdir(storageDir) == False:
+            print("storageDir doesn't exist, aborting.")
             sys.exit(1)
         if os.path.isdir(os.path.join(storageDir,showName)) == False:
+            print("making directory for this show: %s",os.path.join(storageDir,showName))
             os.mkdir(os.path.join(storageDir,showName))
-        copyfile(os.path.join(recordingDir,recordingFile),os.path.join(storageDir,showName,epFilename,os.path.splitext(recordingFile)[-1]))
+        print("copying file")
+        fileDestination = os.path.join(storageDir,showName,epFilename,os.path.splitext(recordingFile)[-1])
+        copyfile(os.path.join(recordingDir,recordingFile),fileDestination)
+        print("file copied to: %s",fileDestination)
         sys.exit(0)
     sys.exit(1)
 
