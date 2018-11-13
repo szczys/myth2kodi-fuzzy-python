2b300525baf6588b2938434955410addfc858ddc
Mike Szczys <szczys@hotmail.com>
2018-11-13 02:14:36 -0600

    File to utilize MythTV python bindings

---
+++ mythPythonBindings.py
@@ -0,0 +1,26 @@
+from MythTV import MythDB
+from MythTV import MythBE
+
+mythConfig = '/home/mythtv/config.xml'
+#mythConfig = '/home/mike/mythtv.xml'
+
+import xml.etree.ElementTree as ET
+
+def deleteProgram(basename)
+    tree = ET.parse(mythConfig)
+    root = tree.getroot()
+
+    db1 = MythDB(args=(('DBHostName',root.find('Database').find('Host').text),
+                       ('DBName',root.find('Database').find('DatabaseName').text),
+                       ('DBUserName',root.find('Database').find('UserName').text),
+                       ('DBPassword',root.find('Database').find('Password').text)))
+
+    be1 = MythBE(db=db1)
+
+    showInfoGen = db1.searchRecorded(basename=basename)
+    showInfo = next(showInfoGen)
+    showObject = be1.getRecording(y['chanid'],y['starttime'])
+
+    deletedShow = be1.deleteRecording(z,force=True) #Return -1 means success
+
+
