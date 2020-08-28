79238677034ea3311cf6a0f5f41f3dde650e064c
Mike Szczys <szczys@hotmail.com>
2020-08-27 22:06:26 -0500

    Standalone python3 to query mythtv python bindings

---
+++ mythObjGetter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from mythPythonBindings import getProgramObjectFromFilename, getDbObject, getBeObject
+import pickle
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    #testfilename="1212_20200824200000.ts"
+
+    targetProgram = getProgramObjectFromFilename(sys.argv[1], getDbObject(), getBeObject())
+
+    returnObj = dict()
+    returnObj['title'] = targetProgram['title']
+    returnObj['airdate'] = targetProgram['airdate']
+    returnObj['subtitle'] = targetProgram['subtitle']
+    if targetProgram == None:
+        sys.exit(1)
+    else:
+        pickle.dump(returnObj, open("showObj.p","wb"))
+        sys.exit(0)
+
+if __name__ == '__main__':
+    main()
