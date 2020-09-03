d509bba53cb86e4dcbef25df46d362ecf25c0b25
Mike Szczys <szczys@hotmail.com>
2020-09-02 22:08:47 -0500

    Cleaning up detritus from development

--- findAndMove.py
+++ findAndMove.py
@@ -1,31 +1,16 @@
-from __future__ import print_function
-
-#from myth2kodiFuzzyPython import findEpisodeFilename, getSeriesName
 from fuzzy_scraper import identifyMythtvEpisode, deleteMythRecording
 import os
 import sys
-import subprocess
 from shutil import copyfile
 from fuzzy_logger import logging
-#from mythPythonBindings import deleteProgram
 
 recordingDir = "/Lenny/mythtv/"
 storageDir = "/Lenny/videoLibrary/TV/"
 
-#This fill will contain filenames of recordings copied to the library for deletion via a cron job (it's hack)
-workingDir = "/home/mythtv/myth2kodi-fuzzy-python/"
-todelete = 'deleteList.txt'
-
 def main():
     recordingFile = sys.argv[1]
-    showName = sys.argv[2]
-    epName = sys.argv[3]
     logging.info("======================")
-    logging.info("Attempting tvdb match to: %s :: %s :: %s", recordingFile, showName, epName)
-##    seriesName = getSeriesName(showName) #Use tvdb series name value for directory names to normalize capitalization
-##    if seriesName == None:
-##        logging.info("Couldn't retrieve series name from tvdb. Aborting.")
-##        sys.exit(1)    
+    logging.info("Attempting tvdb match to: %s :: %s :: %s", recordingFile)
     matchedEpisode = identifyMythtvEpisode(recordingFilename=recordingFile)
     if matchedEpisode != None:
         if os.path.isdir(storageDir) == False:
@@ -43,11 +28,6 @@ def main():
         copyfile(os.path.join(recordingDir,recordingFile),fileDestination)
         os.chmod(fileDestination,0o777)
         logging.info("File copied successfully")
-##        logging.info("Writing filename for deletion by cron job")
-##        with open(workingDir + todelete, 'a') as f:
-##            f.write(recordingFile + '\n')
-##        os.chmod(workingDir + todelete,0o777)
-
         logging.info("Deleting %s",recordingFile)
         deleteMythRecording(recordingFile)
         logging.info("Operation complete. Exiting.")
